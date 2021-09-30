0d8386a235c063ff0602ab3bf73673f60f56813f
yejun_1220@naver.com <yejun_1220@naver.com>
2021-09-30 23:54:06 +0900

    JAVA ORM - JPA(UPDATE 21.09.24)
    섹션 3 - 영속성 관리 - 내부 동작 방식까지

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -11,6 +11,16 @@ public class JpaMain {
 
         EntityManager em = emf.createEntityManager();
 
+        Member member = new Member();
+        member.setId("member1");
+        member.setUsername("회원1");
+
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+
+        //객체를 저장한 상태(영속)
+        em.persist(member);
+
         em.close();
 
         emf.close();
