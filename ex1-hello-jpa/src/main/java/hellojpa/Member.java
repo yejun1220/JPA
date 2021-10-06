93feba6f6d641436ab84902f0f086f2a29ba9f03
yejun_1220@naver.com <yejun_1220@naver.com>
2021-10-06 23:01:15 +0900

    JAVA ORM - JPA(UPDATE 21.10.06)
    섹션 3 - 영속성 관리 - 내부 동작 방식까지

--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -8,7 +8,23 @@ public class Member {
 
     @Id
     private Long id;
-    private String name;
+
+    @Column(name = "name")
+    private String username;
+
+    private Integer age;
+
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    @Lob
+    private String description;
 
     public Long getId() {
         return id;
